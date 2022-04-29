10e3ebcb073c9bde2d457f5f8f12c52601c606f9
kramstyles <mark.eke@decagon.dev>
2022-04-29 02:03:56 +0100

    updated models and installed pep8

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
